196cb23a273436679c5545a70ac616fa7f9ae3d6
Jacklyn Kinsler <jacklyn.kinsler@puppet.com>
2020-12-01 14:14:16 -0800

    Improved logic for tab completion of platforms to be for more specific cases.

--- extras/completions/vanagon.bash
+++ extras/completions/vanagon.bash
@@ -21,14 +21,12 @@ _vanagon()
 
   if [[ $template_arg_commands =~ (^| )$prev($| ) ]] ; then
     if [[ -z "$_vanagon_avail_templates_projects" ]] ; then
-      # TODO: need a --hostnameonly equivalent here because the section headers of
-      # `floaty list` are adding some spurious entries (including files in current
-      # directory because part of the headers is `**` which is getting expanded)
       _vanagon_avail_templates_projects=$(vanagon list -r | sed 1d 2>/dev/null)
       COMPREPLY=( $(compgen -W "${_vanagon_avail_templates_projects}" -- "${cur}") )
     fi
   fi 
-  if [[ ${#COMP_WORDS[@]} -gt 3 && "$(vanagon list -r)" == *"$PREV"* ]] ; then 
+  # there has to be a better way to do this 
+  if [[ ${#COMP_WORDS[@]} -gt 3 && ${COMP_WORDS[1]} == "build" && "$(vanagon list -r)" == *"$PREV"* ]] ; then 
     _vanagon_avail_templates_platforms=$(vanagon list -l | sed 1d 2>/dev/null)
     COMPREPLY=( $(compgen -W "${_vanagon_avail_templates_platforms}" -- "${cur}") )
   fi
