f106752d511fb8c6960298066721dc5b941fef04
Jacklyn Kinsler <jacklyn.kinsler@puppet.com>
2020-12-02 09:02:35 -0800

    Multiple platforms can now be tab completed in bash.

--- extras/completions/vanagon.bash
+++ extras/completions/vanagon.bash
@@ -3,10 +3,9 @@
 _vanagon()
 {
   local cur prev commands template_arg_commands 
-    # -- signifies end of command, necessary here?
 
   # COMREPLY is an array variable used to store completions  
-  # the completion mechanism uses this variable to display its contents as completions
+  # the completion mechanism uses COMPRELY to display its contents as completions
   COMPREPLY=()
   # COMP_WORDS is an array of all the words typed after the name of the program 
   # COMP_CWORD is an index of the COMP_WORDS array pointing to the word the curernt cursor is at
@@ -14,19 +13,17 @@ _vanagon()
   prev="${COMP_WORDS[COMP_CWORD-1]}"
 
   commands="build build_host_info build_requirements inspect list render sign ship help"
-  template_arg_commands="build"
-  
-  # compgen generates completions filtered based on what has been typed by the user
-  # what is happening here?(^| )$prev($| )
+  template_arg_commands="build build_host_info build_requirements inspect render "
 
   if [[ $template_arg_commands =~ (^| )$prev($| ) ]] ; then
     if [[ -z "$_vanagon_avail_templates_projects" ]] ; then
       _vanagon_avail_templates_projects=$(vanagon list -r | sed 1d 2>/dev/null)
+      # compgen generates completions filtered based on what has been typed by the user
       COMPREPLY=( $(compgen -W "${_vanagon_avail_templates_projects}" -- "${cur}") )
     fi
   fi 
-  # there has to be a better way to do this 
-  if [[ ${#COMP_WORDS[@]} -gt 3 && ${COMP_WORDS[1]} == "build" && "$(vanagon list -r)" == *"$PREV"* ]] ; then 
+  # allows multiple platforms to be tab completed 
+  if [[ ${#COMP_WORDS[@]} -gt 3 ]] ; then 
     _vanagon_avail_templates_platforms=$(vanagon list -l | sed 1d 2>/dev/null)
     COMPREPLY=( $(compgen -W "${_vanagon_avail_templates_platforms}" -- "${cur}") )
   fi
@@ -36,5 +33,5 @@ _vanagon()
   fi
 }
 
-# complete with a function `_vanagon` for the commmand `vanagon`
+# assign tab complete function `_vanagon ` to `vanagon` command 
 complete -F _vanagon vanagon
